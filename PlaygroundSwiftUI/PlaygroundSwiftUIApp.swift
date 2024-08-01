37cac980fb1caba0d873ce928560c3b6c6735bb8
prana <prana@synapsis.id>
2024-08-01 21:09:12 +0700

    Initial Commit

---
+++ PlaygroundSwiftUI/PlaygroundSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundSwiftUIApp.swift
+//  PlaygroundSwiftUI
+//
+//  Created by Prana dhika on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
